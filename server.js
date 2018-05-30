148d0f81a1f7cbc1bb4055fe79c02ef6549e2a66
lhrbueno <luishenriquebueno@gmail.com>
2018-05-30 16:15:36 -0300

    Implemented the routes and added db mock
    Added signin, register, profile,entries routes.\nStill missing the password hash.\nStill missing the database.

---
+++ server.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bCrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3003;
+
+const app = express();
+
+const database = {
+  users: [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@email.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 2,
+      name: 'Sally Mustang',
+      email: 'sally@email.com',
+      password: 'cake',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+  res.status(200).json({ users: database.users });
+});
+
+app.post('/signin', (req, res, next) => {
+  if (req.body.email ===  database.users[0].email &&
+      req.body.password === database.users[0].password) {
+    res.status(200).json({ message: 'User logged in succesfully' });
+  } else {
+    res.status(400).json({ message: 'Login or password is incorrect' });
+  }
+});
+
+app.post('/register', (req, res, next) => {
+  if (req.body) {
+    const { name, email, password } = req.body;
+    const userId = database.users.length + 1;
+    const newUser = {
+      id: userId,
+      name: name,
+      email: email,
+      password: password,
+      entries: 0,
+      joined: new Date()
+    };
+
+    database.users.push(newUser);
+    console.log(database.users);
+    res.status(200).json({ message: 'User registered sucessfully' });
+  } else {
+    res.status(500).json({ message: 'An error occurred. Please try again' });
+  }
+});
+
+app.get('/profile/:id', (req, res, next) => {
+  const { id } = req.params;
+  const userDb = database.users.filter((u) => { 
+    return u.id === Number(id);
+  });
+
+  if (userDb != null && userDb.length > 0) {
+    const { id, name, email, entries, joined } = userDb[0];
+    const user = {
+      id: id,
+      name: name,
+      email: email,
+      entries: entries,
+      joined: joined
+    };
+    res.status(200).json({ user: user });
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+
+app.put('/entries', (req, res, next) => {
+  const { id } = req.body;
+  const userDb = database.users.filter((u) => { 
+    return u.id === Number(id);
+  });
+
+  if (userDb != null && userDb.length > 0) {
+    userDb[0].entries++;
+    const { id, name, email, entries, joined } = userDb[0];
+    const user = {
+      id: id,
+      name: name,
+      email: email,
+      entries: entries,
+      joined: joined
+    };
+
+    res.status(200).json({ user: user });
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
