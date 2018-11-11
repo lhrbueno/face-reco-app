60262ebc2ee832ab494191abcaa054d3def7c6bc
lhrbueno <luishenriquebueno@gmail.com>
2018-11-11 17:57:09 -0200

    Added database connection information

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const _CLIENT = 'pg';
+const _HOST = '127.0.0.1';
+const _USER = '';
+const _PASSWORD = '';
+const _DATABASE = ''
+
+const DATABASE_CONNECTION = {
+  client: _CLIENT,
+  connection: {
+    host: _HOST,
+    user: _USER,
+    password: _PASSWORD,
+    database: _DATABASE
+  }
+}
+
+module.exports = DATABASE_CONNECTION;
