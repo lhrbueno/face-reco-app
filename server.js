6d694b7ec1f73c3895b55df4bbbe5ba1ba2d2bcd
lhrbueno <luishenriquebueno@gmail.com>
2018-11-11 22:50:03 -0200

    Added dotenv information and connectec to db using knex dependency

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
+// Server
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const login = require('./src/routes/login');
-const user = require('./src/routes/user');
-
-const mock = require('./src/config/database.mock');
-
-const PORT = process.env.PORT || 3003;
 const app = express();
+const path = require('path');
+
+// Environment variables
+require('dotenv').config({ 
+  path: path.join(__dirname + '/src/config/.env.development.local')
+});
+
+const PORT = process.env.SERVER_PORT;
+
+// Routes
+const loginRoutes = require('./src/routes/login');
+const userRoutes = require('./src/routes/user');
+
+// Database
+const dbConfig = require('./src/config/database');
+const db = require('knex')(dbConfig);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
-  res.status(200).json({ users: mock.users });
+  db
+    .select('*')
+    .from('users')
+    .then(users => {
+      res.status(200).json({ users });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error trying to fetch the users from database'});
+    });
+  
 });
 
-app.use('/api', login);
-app.use('/api', user);
+app.use('/api', loginRoutes);
+app.use('/api', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
