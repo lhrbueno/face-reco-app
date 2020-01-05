c687cc40e722f8cebe28116ec48d94ac3dd4ef6e
lhrbueno <luishenriquebueno@gmail.com>
2020-01-05 10:10:09 -0200

    Moved env var to root directory

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 // Server
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 // Environment variables
-require('dotenv').config({ 
-  path: path.join(__dirname + '/src/config/.env.development.local')
+require("dotenv").config({
+  path: path.join(__dirname + "/.env.development.local")
 });
 
 const PORT = process.env.SERVER_PORT;
 
 // Routes
-const loginRoutes = require('./src/routes/login');
-const userRoutes = require('./src/routes/user');
+const loginRoutes = require("./src/routes/login");
+const userRoutes = require("./src/routes/user");
 
 // Database
-const dbConfig = require('./src/config/database');
-const db = require('knex')(dbConfig);
+const dbConfig = require("./src/config/database");
+const db = require("knex")(dbConfig);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  db
-    .select('*')
-    .from('users')
+app.get("/", (req, res) => {
+  db.select("TOP 1")
+    .from("users")
     .then(users => {
       res.status(200).json({ users });
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({ message: 'Error trying to fetch the users from database'});
+      res
+        .status(500)
+        .json({ message: "Error trying to fetch the users from database" });
     });
-  
 });
 
-app.use('/api', loginRoutes);
-app.use('/api', userRoutes);
+app.use("/api", loginRoutes);
+app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
