462a0f63e64706c6575e7acd3df4ba4e12cab915
lhrbueno <luishenriquebueno@gmail.com>
2020-01-30 21:57:38 -0200

    Added register fn

--- src/repositories/loginRepository.js
+++ src/repositories/loginRepository.js
@@ -1,15 +1,15 @@
 const dbConfig = require('../config/database');
 const db = require('knex')(dbConfig);
 const PasswordUtils = require('../utils/passwordHandler');
+const { PersistenceError, RuntimeError } = require('../utils/errorHandler');
+const { CONSTANTS } = require('../utils/messages');
 
 module.exports.login = async (email, password) => {
   try {
     const [login] = await db
-      .select('*')
-      .from('login')
+      .select(CONSTANTS.SELECT_ALL)
+      .from(CONSTANTS.TABLES.LOGIN)
       .where({ email });
-
-    console.log(login);
 
     const isPasswordValid = await PasswordUtils.comparePassword(
       password,
@@ -18,19 +18,50 @@ module.exports.login = async (email, password) => {
 
     return isPasswordValid ? getUserByEmail(email) : {};
   } catch (err) {
-    throw new Error(err);
+    if (err && err.type) {
+      throw RuntimeError(err.type, err.message);
+    }
+
+    throw PersistenceError(err.message);
   }
 };
 
 const getUserByEmail = async email => {
   try {
     const [user] = await db
-      .select('*')
-      .from('users')
-      .where('email', '=', email);
+      .select(CONSTANTS.SELECT_ALL)
+      .from(CONSTANTS.TABLES.USERS)
+      .where({ email });
 
     return user;
   } catch (err) {
-    throw new Error(err);
+    throw PersistenceError(err.message);
+  }
+};
+
+module.exports.register = async user => {
+  try {
+    const { name, email, password } = user;
+
+    const userDB = await db(CONSTANTS.TABLES.USERS).insert({
+      name,
+      email,
+      joined: new Date()
+    });
+
+    const hashedPwd = await PasswordUtils.hashPassword(password);
+
+    await db(CONSTANTS.TABLES.LOGIN).insert({
+      email,
+      hash: hashedPwd
+    });
+
+    return userDB;
+  } catch (err) {
+    if (err && err.type) {
+      throw RuntimeError(err.type, err.message);
+    }
+
+    throw PersistenceError(err.message);
   }
 };
