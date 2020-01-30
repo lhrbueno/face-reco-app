58c04f5925e1cb5243b406d33f4aa1e7d6a6838f
lhrbueno <luishenriquebueno@gmail.com>
2020-01-30 21:56:52 -0200

    Added error messages and tables names

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,11 +1,27 @@
-export const HTTP_RESPONSE = {
-  OK: {
-    STATUS: 200,
-    MESSAGE: 'OK'
+module.exports.ERROR = {
+  DEFAULT: {
+    type: 'Server Error',
+    message: 'Something went wrong. Try again in few minutes'
   },
-  CREATED: {
-    STATUS: 201,
-    MESSAGE: 'CREATED'
+  UNAUTHORIZED: {
+    type: 'Unauthorized request',
+    message: 'Unauthorized user'
+  },
+  PERSISTENCE: {
+    type: 'Persistence Error',
+    message: 'Something went wrong trying to mess with the DB'
+  },
+  PASSWORD: {
+    type: 'BCryptJS Password Error',
+    message: 'Something went wrong with BCrypt'
   }
-  // TODO
+};
+
+module.exports.CONSTANTS = {
+  SELECT_ALL: '*',
+  SALT_NUMBER: 13,
+  TABLES: {
+    USERS: 'users',
+    LOGIN: 'login'
+  }
 };
