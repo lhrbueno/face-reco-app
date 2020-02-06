15d0702b300918fb5acedddd2596b6396fb39bf1
lhrbueno <luishenriquebueno@gmail.com>
2020-02-06 01:49:22 -0300

    Updated password field on seed

--- src/config/scripts/seed/seed.sql
+++ src/config/scripts/seed/seed.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (name, email, entries, joined) VALUES ('admin', 'admin@test.com', 999, CURRENT_DATE);
 
-INSERT INTO login (hash, email) VALUES ('$2a$17$mA4a2FH6ooMPIl58b4BMl.YCDjPnymJiZlPngSl282VMmdwfU8SGS', 'admin@test.com');
+INSERT INTO login (hash, email) VALUES ('$2a$13$1gG5S/pwcIRKwwPa0RSCv.dbd7L8geaGscE.mVvmOBdfIyPSx11Q.', 'admin@test.com');
