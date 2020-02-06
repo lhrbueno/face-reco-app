0a453c48d0c7283dcd2a94bddc6552881f0370be
lhrbueno <luishenriquebueno@gmail.com>
2020-02-06 01:25:08 -0300

    Moved URI to docker-compose

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,6 @@
-const CONN = `${process.env.DB_VENDOR}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DATABASE_NAME}`;
-
 const DATABASE_CONNECTION = {
   client: process.env.DB_CLIENT,
-  connection: CONN,
+  connection: process.env.POSTGRES_URI,
   pool: {
     min: parseInt(process.env.DB_MIN_POOL_CONNECTION),
     max: parseInt(process.env.DB_MAX_POOL_CONNECTION)
